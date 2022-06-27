49717719489b78465e509e5b3fe1d39b66700828
Ts-zitha <msipho351@gmail.com>
2022-06-27 21:49:37 +0200

    altered the controller and added more routes

--- controllers/business.controller.js
+++ controllers/business.controller.js
@@ -1,9 +1,12 @@
+const { json } = require('body-parser');
+const businessModel = require('../models/business.model');
 const Business = require('../models/business.model');
+const categoryModel = require('../models/category.model');
+const City = require('../models/city.model');
 
 
 //getting all business
-exports.getBusiness = async (req, res)=>{
-
+exports.getBusiness = (req, res)=>{
     Business.find()
     .populate('city', 'city_name')
     .populate('category', 'type')
@@ -23,24 +26,91 @@ exports.createBusiness = (req, res)=>{
     .then(data=> res.status(200).json({msg : "business create", data}))
     .catch(err => res.status(500).json({msg:"error while creating business in the DD", err}));
 };
-//get businesses by category
+//get businesses by category//join by category
 exports.getBusinessByCategory = (req, res)=>{
-    
-    Business.find().where('category', '_id')
-    .equals(req.params.id)
-    .select('name')
-    .populate('category')
-    .populate('city description est') 
-    .exec()
-    .then(data=> {
-        if(data.length == 0){
-            res.status(404).json({ msg : 'Category not mapped to any Business'});
-        }else{
-            res.status(200).json(data);
-        }
-    })
-    .catch(err=> res.status(500).json({msg: 'error while getting businesses by category'}))
+    categoryModel.find()
+    .where('type')
+    .equals(req.params.cat_name)
+    .then(category =>{
+        cat_id = category[0]._id;
+        Business.find().where('category', '_id')
+        .equals(cat_id)
+        .select('name  description est')
+        .populate('category', 'type')
+        .populate('city', 'city_name')
+        .exec()
+        .then(data=>{
+            if(data.length == 0){
+                res.status(200).json({msg: 'No busineses mapped with that category'})
+            }else{
+                res.status(200).json(data);
+            }}).catch(err=>{
+            res.status(500).json({msg: 'Error while while fetching business in that category'});
+        });
+    }).catch(err=>{
+        res.status(404).json({ msg: 'category does not exists '});
+    });
 };
+//join business by city
+exports.getBusinessByCity = (req, res)=>{
+    City.find()
+    .where('city_name')
+    .equals(req.params.city_name)
+    .then(city =>{
+        city_id = city[0]._id;
+        Business.find().where('city', '_id')
+        .equals(city_id)
+        .select('name  description est')
+        .populate('category', 'type')
+        .populate('city', 'city_name')
+        .exec()
+        .then(data=>{
+            if(data.length == 0){
+                res.status(200).json({msg: 'No busineses mapped with this city'});
+            }else{
+                res.status(200).json(data);
+            }}).catch(err=>{
+                res.status(500).json({msg: 'Error while while fetching business in that city'});
+            });
+    }).catch(err=>{
+        res.status(404).json({msg:'City does not exist from collection'});
+    });
+}
+
+//join by city and category
+exports.getBusinessByCategoryCity= (req, res)=>{
+    City.find()
+    .where('city_name')
+    .equals(req.params.city)
+    .then(city =>{
+        categoryModel.find().where('type')
+        .equals(req.params.cat)
+        .then(category=>{
+            city_id = city[0]._id;
+            cat_id = category[0]._id;
+            Business.find().where('city', '_id')
+            .equals(city_id)
+            .where('category', '_id')
+            .equals(cat_id)
+            .select('name  description est')
+            .populate('category', 'type')
+            .populate('city', 'city_name')
+            .exec()
+            .then(data =>{
+                if(data.length == 0){
+                    res.status(200).json({msg:`No business mapped with ${req.params.city} city and ${req.params.cat} category`})
+                }else{
+                    res.status(200).json(data);
+                }}).catch(err=>{
+                res.status(500).json({msg:`error while getting businesses mapped to ${req.param.cat} category and ${req.param.city}`})
+            });
+        }).catch(err=>{
+            res.status(404).json({msg: 'Category or City  does not exist from collection'});
+        });
+    }).catch(err=>{
+        res.status(404).json(err);
+    });
+}
 
 //get Business byId
 exports.getBusinessById = (req, res)=>{
@@ -53,7 +123,7 @@ exports.getBusinessById = (req, res)=>{
         }
     }).catch(err=>{
         if(err.name == 'CastError'){
-            res.status(404).json({ msg : 'Error id type'})
+            res.status(404).json({ msg : 'Error id type'});
         }else{
             res.status(500).json({msg : err})
         }
--- routes/business.route.js
+++ routes/business.route.js
@@ -8,7 +8,9 @@ const router = express.Router();
 //route to get all business in the DB
 router.get('/', businessController.getBusiness);
 router.post('/', businessController.createBusiness);
-router.get('/businessBycategory/:id', businessController.getBusinessByCategory);
+router.get('/businessBycategory/:cat_name', businessController.getBusinessByCategory);
+router.get('/businessByCity/:city_name', businessController.getBusinessByCity);
+router.get('/businessByCityCategory/:city/:cat', businessController.getBusinessByCategoryCity);
 router.get('/:id', businessController.getBusinessById);
 router.put('/:id', businessController.updateBusiness);
 
